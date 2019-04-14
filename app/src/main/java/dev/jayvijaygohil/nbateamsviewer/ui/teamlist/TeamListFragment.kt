c9b1d3f8c028432ca07778ee8db6c00b7c3e917b
jayvijaygohil <jayvijaygohildev@outlook.com>
2019-04-14 11:55:18 -0400

    Don't allow to download the list again if the fragment's state is already present

--- app/src/main/java/dev/jayvijaygohil/nbateamsviewer/ui/teamlist/TeamListFragment.kt
+++ app/src/main/java/dev/jayvijaygohil/nbateamsviewer/ui/teamlist/TeamListFragment.kt
@@ -18,6 +18,7 @@ import dev.jayvijaygohil.nbateamsviewer.ui.SingleActivityContract
 import dev.jayvijaygohil.nbateamsviewer.ui.teamsort.TeamSortDialogFragment.Companion.SORT_RESULT_CODE
 import dev.jayvijaygohil.nbateamsviewer.ui.teamsort.TeamSortDialogFragment.Companion.SORT_RESULT_INTENT_KEY
 import dev.jayvijaygohil.nbateamsviewer.usecase.SortTeamsUseCase.SortType
+import dev.jayvijaygohil.nbateamsviewer.usecase.SortTeamsUseCase.SortType.NAME_ASC
 import kotlinx.android.synthetic.main.fragment_team_list.btn_error_retry
 import kotlinx.android.synthetic.main.fragment_team_list.error_container
 import kotlinx.android.synthetic.main.fragment_team_list.rv_all_team_list
@@ -39,6 +40,12 @@ class TeamListFragment : DaggerFragment(), TeamListContract.View {
     @Inject
     lateinit var adapter: TeamListAdapter
 
+    private var lastKnownSortType: SortType = NAME_ASC
+    private var isNewFragmentInstance: Boolean = true
+
+    private val TEAM_LIST_BUNDLE_KEY = "team_list"
+    private val LAST_KNOWN_SORT_TYPE_BUNDLE_KEY = "last_known_sort_type"
+
     override fun onAttach(context: Context) {
         component().injectTeamListFragment(this)
         super.onAttach(context)
@@ -57,17 +64,30 @@ class TeamListFragment : DaggerFragment(), TeamListContract.View {
         setupSwipeToRefresh()
         setupRetry()
 
-        if (savedInstanceState == null) {
+        if (savedInstanceState != null) {
+            isNewFragmentInstance = false
+            restoreState(savedInstanceState)
+        } else if (isNewFragmentInstance) {
+            isNewFragmentInstance = false
             presenter.fetchTeams()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
         if (requestCode == SORT_REQUEST_CODE && resultCode == SORT_RESULT_CODE && data != null) {
             val sortTypeOrdinal = data.getIntExtra(SORT_RESULT_INTENT_KEY, 0)
-            val sortType = SortType.values()[sortTypeOrdinal]
-            presenter.sortTeams(adapter.getTeamList(), sortType)
+            lastKnownSortType = SortType.values()[sortTypeOrdinal]
+            presenter.sortTeams(adapter.getTeamList(), lastKnownSortType)
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putParcelableArrayList(TEAM_LIST_BUNDLE_KEY, ArrayList(adapter.getTeamList()))
+        outState.putInt(LAST_KNOWN_SORT_TYPE_BUNDLE_KEY, lastKnownSortType.ordinal)
     }
 
     override fun showTeams(list: List<Team>) {
@@ -124,6 +144,18 @@ class TeamListFragment : DaggerFragment(), TeamListContract.View {
     private fun setupRetry() {
         btn_error_retry.setOnClickListener {
             presenter.fetchTeams()
+        }
+    }
+
+    private fun restoreState(savedInstanceState: Bundle) {
+        if (savedInstanceState.containsKey(TEAM_LIST_BUNDLE_KEY)) {
+            val teamList = savedInstanceState.getParcelableArrayList<Team>(TEAM_LIST_BUNDLE_KEY)!!.toList()
+            adapter.addNewTeamList(teamList)
+        }
+
+        if (savedInstanceState.containsKey(LAST_KNOWN_SORT_TYPE_BUNDLE_KEY)) {
+            val sortTypeOrdinal = savedInstanceState.getInt(LAST_KNOWN_SORT_TYPE_BUNDLE_KEY)
+            lastKnownSortType = SortType.values()[sortTypeOrdinal]
         }
     }
 
